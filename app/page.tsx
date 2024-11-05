7e9764d254ae2c1dcab6b38fe4de6e0cbf7fba1f
LeonardoIrineu <leo_izidoro_@hotmail.com>
2024-11-05 20:12:03 -0300

    chore: add prisma and database setup

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return ( <h1> Helo World </h1> );
+};
+
+export default Home;
---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const subscription = () => {
+    return (<h1> subscription </h1>);
+};
+
+export default subscription;
